e4af9aa3e60d34887ce2d65de02ff34d094baa4d
Fadriano91 <fquimico@hotmail.com.br>
2022-03-31 17:46:21 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,30 +1,17 @@
-﻿ string opcaoUsuario = ObterOpcaoUsuario();
-
-switch (opcaoUsuario.ToUpper())
-               {
-                    case "M":
-                        System.Console.WriteLine("Bom Dia");
-                        break;
-                    case "V":
-                        System.Console.WriteLine("Boa Tarde");                   
-                        break;
-                    case "N":
-                        System.Console.WriteLine("Boa Noite");
-                        break;
-                    default:
-                    throw new ArgumentOutOfRangeException();                
-               }
-    opcaoUsuario = ObterOpcaoUsuario();
-
-string ObterOpcaoUsuario()
-{
-    Console.WriteLine();
-            Console.WriteLine("Favor digitar o turno que você trabalha:");
-            Console.WriteLine("Sendo M-matutino");
-            Console.WriteLine("      V-vespertino");
-            Console.WriteLine("      N-noturno");            
-            string opcaoUsuario = Console.ReadLine().ToUpper();
-            Console.WriteLine();
-            return opcaoUsuario;
+﻿Console.WriteLine("Favor digitar o turno que você trabalha:");
+Console.WriteLine("Sendo M-matutino");
+Console.WriteLine("      V-vespertino");
+Console.WriteLine("      N-noturno"); 
+Console.WriteLine("Digite a opção:"); 
+var turno = Console.ReadLine();
+if( turno == "M"){
+    Console.WriteLine("Bom Dia");
+}else if(turno == "V"){
+    Console.WriteLine("Boa Tarde");
+}else if(turno =="N"){
+    Console.WriteLine("Boa Noite");
+}else{
+    Console.WriteLine("Valor Inválido");
 }
+            
 
