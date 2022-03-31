d510d36549041021d487c331d53e8346218a04ec
Fadriano91 <fquimico@hotmail.com.br>
2022-03-31 17:27:04 -0300

    Confirm Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿ string opcaoUsuario = ObterOpcaoUsuario();
+
+switch (opcaoUsuario.ToUpper())
+               {
+                    case "M":
+                        System.Console.WriteLine("Bom Dia");
+                        break;
+                    case "V":
+                        System.Console.WriteLine("Boa Tarde");                   
+                        break;
+                    case "N":
+                        System.Console.WriteLine("Boa Noite");
+                        break;
+                    default:
+                    throw new ArgumentOutOfRangeException();                
+               }
+    opcaoUsuario = ObterOpcaoUsuario();
+
+string ObterOpcaoUsuario()
+{
+    Console.WriteLine();
+            Console.WriteLine("Favor digitar o turno que você trabalha:");
+            Console.WriteLine("Sendo M-matutino");
+            Console.WriteLine("      V-vespertino");
+            Console.WriteLine("      N-noturno");            
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+}
+
